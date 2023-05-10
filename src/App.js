57cb11a9ef24ac948c5335f41eff41546f999624
Riley <94302477+Rilaey@users.noreply.github.com>
2023-05-09 21:27:08 -0400

    worked on home page along with navigating pages.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import AboutMe from './Pages/AboutMe'
+import ContactMe from './Pages/ContactMe'
+import Projects from './Pages/Projects'
+import Resume from './Pages/Resume'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/AboutMe" element={<AboutMe />} />
+        <Route path="/ContactMe" element={<ContactMe />} />
+        <Route path="/Projects" element={<Projects />} />
+        <Route path="/Resume" element={<Resume />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,52 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import DarkBgImage from "../images/dark-bg-img.jpg";
 
 export default function Home() {
+  const navigate = useNavigate();
+
   return (
-    <div>Home</div>
-  )
+    <div
+      style={{
+        backgroundImage: `url(${DarkBgImage})`,
+        height: "100vh",
+        width: "100%",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        textAlign: "center",
+        paddingBottom: "20%"
+      }}
+    >
+      <div>
+        <h1 className="my-10 home-header">Welcome To My Portfolio</h1>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          textAlign: "center",
+          alignContent: "center"
+        }}
+      >
+        <div
+          className="home-buttons"
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+          }}
+        >
+          <button className="btn btn-outline m-4" onClick={() => navigate('/AboutMe')}>About Me</button>
+          <button className="btn btn-outline m-4" onClick={() => navigate('/ContactMe')}>Contact Me</button>
+          <button className="btn btn-outline m-4" onClick={() => navigate('/Projects')}>Projects</button>
+          <button className="btn btn-outline m-4" onClick={() => navigate('/Resume')}>Resume</button>
+        </div>
+      </div>
+    </div>
+  );
 }
