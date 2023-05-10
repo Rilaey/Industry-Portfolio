24462576c93f29342729a82dfcfacced1949abb5
Riley <94302477+Rilaey@users.noreply.github.com>
2023-05-10 18:00:51 -0400

    more work on homepage with social icons.

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import DarkBgImage from "../images/dark-bg-img.jpg";
+import GitHubLogo from "../images/github-logo.png";
+import LinkedInLogo from "../images/linkedin-logo.png";
+import TwitterLogo from "../images/twitter-white-logo.png";
 
 export default function Home() {
   const navigate = useNavigate();
@@ -22,7 +25,7 @@ export default function Home() {
       }}
     >
       <div>
-        <h1 className="my-10 home-header">Welcome To My Portfolio</h1>
+        <h1 className="my-10 home-header">Riley Newhart's Portfolio</h1>
       </div>
       <div
         style={{
@@ -38,13 +41,62 @@ export default function Home() {
           className="home-buttons"
           style={{
             display: "flex",
-            flexWrap: "wrap",
+            flexWrap: "wrap"
           }}
         >
-          <button className="btn btn-outline m-4" onClick={() => navigate('/AboutMe')}>About Me</button>
-          <button className="btn btn-outline m-4" onClick={() => navigate('/ContactMe')}>Contact Me</button>
-          <button className="btn btn-outline m-4" onClick={() => navigate('/Projects')}>Projects</button>
-          <button className="btn btn-outline m-4" onClick={() => navigate('/Resume')}>Resume</button>
+          <button
+            className="btn btn-outline m-4"
+            onClick={() => navigate("/AboutMe")}
+          >
+            About Me
+          </button>
+          <button
+            className="btn btn-outline m-4"
+            onClick={() => navigate("/ContactMe")}
+          >
+            Contact Me
+          </button>
+          <button
+            className="btn btn-outline m-4"
+            onClick={() => navigate("/Projects")}
+          >
+            Projects
+          </button>
+          <button
+            className="btn btn-outline m-4"
+            onClick={() => navigate("/Resume")}
+          >
+            Resume
+          </button>
+        </div>
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          textAlign: "center"
+        }}
+      >
+        <div>
+          <a href="https://github.com/Rilaey">
+            <img src={GitHubLogo} alt="github logo" className="social-logo" />
+          </a>
+        </div>
+        <div>
+          <a href="https://www.linkedin.com/in/riley-newhart-667b43128/">
+            <img
+              src={LinkedInLogo}
+              alt="linkedin logo"
+              className="social-logo"
+            />
+          </a>
+        </div>
+        <div>
+          <a href="https://twitter.com/codingWithRiley">
+            <img src={TwitterLogo} alt="twitter logo" className="social-logo"/>
+          </a>
         </div>
       </div>
     </div>
