f14bef6573adff0e00ad4a6031245bd7460dc24b
Riley <94302477+Rilaey@users.noreply.github.com>
2023-07-05 17:35:37 -0400

    project pictures ui fix.

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import "../index.css";
 import GitHubLogo from "../images/github-logo.png";
 import LinkedInLogo from "../images/linkedin-logo.png";
